4fd7cc8129dd4742015dbc969d466588a06342c6
Zuhayer Kalam <nabil.kalam@gmail.com>
2025-06-09 04:11:05 -0500

    m

--- tests/thermal_model/test_heat_transfer.py
+++ tests/thermal_model/test_heat_transfer.py
@@ -8,8 +8,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 import unittest
 from unittest.mock import patch
 import warnings
-from .heat_transfer_data import *
-from .heat_transfer import *
+from thermal_model.heat_transfer_data import *
+from thermal_model.heat_transfer import *
 
 
 class TestPhysicalConstants(unittest.TestCase):
